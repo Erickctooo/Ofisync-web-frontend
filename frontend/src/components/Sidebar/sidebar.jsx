e80250c8ed01f125d9296974ca3ec69c1f07ed81
Erick San Martin <erick20sanmartin.z@gmail.com>
2025-10-15 19:33:45 -0300

    Mejoras Visuales

--- frontend/src/components/Sidebar/sidebar.jsx
+++ frontend/src/components/Sidebar/sidebar.jsx
@@ -1,22 +1,56 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// Importa NavLink para detectar la ruta activa
+import { NavLink } from "react-router-dom";
 import "./sidebar.css";
+
+// Un componente simple para los iconos (puedes reemplazarlo con una librería como React Icons)
+const Icon = ({ src }) => <img src={src} alt="" className="nav-icon" />;
 
 function Sidebar() {
   return (
     <aside className="sidebar">
-      <ul>
-        <li><Link to="/Inicio">Inicio</Link></li>
-        <li><a href="">Dashboard</a></li>
-        <li><Link to="/Administracion">Administracion</Link></li>
-        <li><a href="">Cuentas</a></li>
-        <li><Link to="/GastoComun">Gasto Comun</Link></li>
-        <li><a href="">Informacion</a></li>
-        <div className="logout">
-            <img src="/img/logout.jpg" alt="" />
-            <li><a href="">Cerrar Sección</a></li>
-        </div>
-      </ul>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/Inicio" className={({ isActive }) => (isActive ? "active" : "")}>
+              <Icon src="/img/icons/home.svg" />
+              <span>Inicio</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard" className={({ isActive }) => (isActive ? "active" : "")}>
+              <Icon src="/img/icons/dashboard.svg" />
+              <span>Dashboard</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/Administracion" className={({ isActive }) => (isActive ? "active" : "")}>
+              <Icon src="/img/icons/admin.svg" />
+              <span>Administración</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/cuentas" className={({ isActive }) => (isActive ? "active" : "")}>
+              <Icon src="/img/icons/accounts.svg" />
+              <span>Cuentas</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/GastoComun" className={({ isActive }) => (isActive ? "active" : "")}>
+              <Icon src="/img/icons/expenses.svg" />
+              <span>Gasto Común</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/informacion" className={({ isActive }) => (isActive ? "active" : "")}>
+              <Icon src="/img/icons/info.svg" />
+              <span>Información</span>
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      
     </aside>
   );
 }
